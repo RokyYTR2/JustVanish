0bcbb403f7a2d2a42dba2526685041eb6b53d7ab
rokyytr.official <163911146+RokyYTR2@users.noreply.github.com>
2025-12-11 16:30:13 +0100

    Update VersionChecker.java

--- src/main/java/dev/meyba/justVanish/utils/VersionChecker.java
+++ src/main/java/dev/meyba/justVanish/utils/VersionChecker.java
@@ -34,7 +34,7 @@ public class VersionChecker {
                     plugin.getLogger().warning("Download: https://github.com/" + githubUser + "/" + repoName);
                     plugin.getLogger().warning("================================================");
                 } else if (latestVersion != null) {
-                    plugin.getLogger().info("You are running the latest version!");
+                    plugin.getLogger().info("You are running the latest version! (Version: " + currentVersion + ")");
                 }
             } catch (Exception e) {
                 plugin.getLogger().log(Level.WARNING, "Failed to check for updates: " + e.getMessage());
@@ -65,7 +65,6 @@ public class VersionChecker {
                 while ((line = reader.readLine()) != null) {
                     if (line.startsWith("version:")) {
                         String version = line.substring(line.indexOf(":") + 1).trim();
-                        // Remove quotes or apostrophes if present
                         version = version.replaceAll("['\"]", "");
 
                         reader.close();
